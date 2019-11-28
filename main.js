f6ca6606edfdc476ee922895a30f99c2f19a309a
UmbertoV <umberto.ottone@gmail.com>
2019-11-28 12:23:09 +0100

    JS Finito

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
+    $(".next").click(function(){
+        var imgCorrente = $("img.active");
+        var imgSuccessiva = imgCorrente.next();
+        if (imgSuccessiva.length == 0) {
+            imgSuccessiva = $(".prima");
+        }
+        imgCorrente.removeClass("active");
+        imgSuccessiva.addClass("active");
+    })
+    $(".preview").click(function(){
+        var imgCorrente = $("img.active");
+        var imgPrecedente = imgCorrente.prev();
+        if (imgPrecedente.length == 0) {
+            imgPrecedente = $(".ultima");
+        }
+        imgCorrente.removeClass("active");
+        imgPrecedente.addClass("active");
+    })
 
 });
